350afa0936febca7cfeece764e85924d4269f511
wdwsbn <wdwsbn@sina.com>
2018-08-29 18:40:15 +0800

    RandomMonkeyAlgorithym

--- ResistAutoCalculate/Program.cs
+++ ResistAutoCalculate/Program.cs
@@ -14,29 +14,93 @@ namespace ResistAutoCalculate
     {
         static void Main(string[] args)
         {
+            const double pi = 3.14159;
+            Random rd = new Random();
+            MWNumericArray nPole = 1;
+            MWNumericArray sPole = 2;
             CRD crd = new CRD();
-            Console.Write("Please input scale ");
-            int scale = Convert.ToInt32(Console.ReadLine());
+            int scale = 10;
+            double min = 99999;
             int i = 0;
-            int n = scale * scale;
-            double[] a = new double[n];
-
-            for (i = 0; i < n; i++)
+            int j = 0;
+            int k = 0;
+            double[] rInBox = { 0, -1, -0.5, -0.2 };
+            double[,] aMatOut = new double[scale, scale];
+            for (k = 0; k < 1000; k++)
             {
-                Console.Write("Please input Admittance matrix ");
-                a[i] = Convert.ToDouble(Console.ReadLine());
+                int horizon = scale * scale;
+                double[] aArr = new double[horizon];
+                double[] nod = new double[horizon];
+                double[,] aMatInMatrix = new double[scale, scale];
+                for (i = 0; i < scale; i++)
+                {
+                    for (j = 0; j < scale; j++)
+                    {
+                        if (i != j)
+                        {
+                            int rad = rd.Next(0, 4);
+                            System.Threading.Thread.Sleep(1);
+                            aMatInMatrix[i, j] = rInBox[rad];
+                            nod[i] = nod[i] - aMatInMatrix[i, j];
+                        }
+                    }
+                }
+                for (i = 0; i < scale; i++)
+                {
+                    aMatInMatrix[i, i] = nod[i];
+                }
 
+                MatrixToArr(scale, aArr, aMatInMatrix);
+                double resist = Double.Parse(crd.CalculateResistanceDistance(scale, aArr, nPole, sPole));
+                double now = Math.Abs(resist - pi);
+                if (now < min)
+                {
+                    min = now;
+                    for (i = 0; i < scale; i++)
+                    {
+                        for (j = 0; j < scale; j++)
+                        {
+                            aMatOut[i, j] = aMatInMatrix[i,j];
+                        }
+                    }
+                }
+               
             }
-            Console.Write("Please input node1 ");
-            MWNumericArray nPole = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Please input node2 ");
-            MWNumericArray sPole = Convert.ToInt32(Console.ReadLine());
-            string s = crd.CalculateResistanceDistance(scale, a, nPole, sPole);
-            Console.Write("Resistance distance is ");
-            Console.Write(s);
-            Console.ReadKey();
+            for (i = 0; i < scale; i++)
+            {
+                for (j = 0; j < scale; j++)
+                {
+                    if ((j + 1) == scale)
+                    {
+                        Console.Write(aMatOut[i, j] + "\n");
+                    }
+                    else
+                    {
+                        Console.Write(aMatOut[i, j] + " ");
+                    }
+                }
+            }
+            Console.Write("最小偏差"+min);
+
+
+
+                    Console.ReadKey();
         }
 
-     
+        private static void MatrixToArr(int scale, double[] aMat, double[,] aMatInMatrix)
+        {
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            for (a = 0; a < scale; a++)
+            {
+                for (b = 0; b < scale; b++)
+                {
+                    aMat[c] = aMatInMatrix[a, b];
+                    c++;
+                }
+            }
+        }
+
     }
 }
