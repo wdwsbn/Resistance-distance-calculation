80b579e4d3b32d094b2fb34361ab2401333b1eb2
wdwsbn <wdwsbn@sina.com>
2018-08-29 17:39:13 +0800

    make it more OO

---
+++ ResistAutoCalculate/CRD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ResistCalculate; //添加引用
+using MathWorks.MATLAB.NET.Arrays;
+namespace ResistAutoCalculate
+{
+    class CRD
+    {
+        public string CalculateResistanceDistance(int scale, double[] a, MWNumericArray nPole, MWNumericArray sPole)
+        {
+            MWArray A = new MWNumericArray(scale, scale, a);
+            ResisrCalcu resisrCalcu = new ResisrCalcu();
+            string s = resisrCalcu.Resist(A, nPole, sPole).ToString();
+            return s;
+        }
+    }
+}
--- ResistAutoCalculate/Program.cs
+++ ResistAutoCalculate/Program.cs
@@ -14,27 +14,29 @@ namespace ResistAutoCalculate
     {
         static void Main(string[] args)
         {
-            Console.Write("Please input scale");
+            CRD crd = new CRD();
+            Console.Write("Please input scale ");
             int scale = Convert.ToInt32(Console.ReadLine());
             int i = 0;
             int n = scale * scale;
             double[] a = new double[n];
-           
-            for (i = 0; i < n; i++) {
-                Console.Write("Please input Admittance matrix");
-                a[i] = Convert.ToDouble(Console.ReadLine()) ;
+
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("Please input Admittance matrix ");
+                a[i] = Convert.ToDouble(Console.ReadLine());
 
             }
-            Console.Write("Please input node1");
+            Console.Write("Please input node1 ");
             MWNumericArray nPole = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Please input node2");
+            Console.Write("Please input node2 ");
             MWNumericArray sPole = Convert.ToInt32(Console.ReadLine());
-            MWArray A = new MWNumericArray(scale,scale,a);
-            ResisrCalcu resisrCalcu = new ResisrCalcu();
-            string s = resisrCalcu.Resist(A, nPole, sPole).ToString();
-            Console.Write("Resistance distance is");
+            string s = crd.CalculateResistanceDistance(scale, a, nPole, sPole);
+            Console.Write("Resistance distance is ");
             Console.Write(s);
             Console.ReadKey();
         }
+
+     
     }
 }
