500b439e3e85132c13e47202cbdf2a5ec86dd0a0
wdwsbn <wdwsbn@sina.com>
2018-08-30 11:38:03 +0800

    优化

--- ResistAutoCalculate/Program.cs
+++ ResistAutoCalculate/Program.cs
@@ -18,15 +18,20 @@ namespace ResistAutoCalculate
             Random rd = new Random();
             MWNumericArray nPole = 1;
             MWNumericArray sPole = 2;
+            double resistout=0;
+            int i1 = 0;
+            int j1 = 0;
+            int nPolego = 0;
+            int sPolego = 0;
             CRD crd = new CRD();
-            int scale = 10;
+            int scale = 7;
             double min = 99999;
             int i = 0;
             int j = 0;
             int k = 0;
-            double[] rInBox = { 0, -1, -0.5, -0.2 };
+            double[] rInBox = { 0, -1, -0.5, -0.2 ,0,0};
             double[,] aMatOut = new double[scale, scale];
-            for (k = 0; k <2000; k++)
+            for (k = 0; k <11100; k++)
             {
                 Console.WriteLine(k);
                 Console.WriteLine(min);
@@ -36,16 +41,17 @@ namespace ResistAutoCalculate
                 double[,] aMatInMatrix = new double[scale, scale];
                 for (i = 0; i < scale; i++)
                 {
-                    for (j = 0; j < scale; j++)
+                    for (j = i; j < scale; j++)
                     {
                         if (i != j)
                         {
-                            int rad = rd.Next(0, 4);
+                            int rad = rd.Next(0, 6);
                             System.Threading.Thread.Sleep(1);
                             
                             aMatInMatrix[i, j] = rInBox[rad];
                             aMatInMatrix[j, i] = aMatInMatrix[i, j];
-                            nod[i] = nod[i] - 2*aMatInMatrix[i, j];
+                            nod[i] = nod[i] - aMatInMatrix[i, j];
+                            nod[j] = nod[j] - aMatInMatrix[i, j];
                         }
                     }
                 }
@@ -55,18 +61,42 @@ namespace ResistAutoCalculate
                 }
 
                 MatrixToArr(scale, aArr, aMatInMatrix);
-                double resist = Double.Parse(crd.CalculateResistanceDistance(scale, aArr, nPole, sPole));
-                double now = Math.Abs(resist - pi);
-                if (now < min)
+                try
                 {
-                    min = now;
-                    for (i = 0; i < scale; i++)
+                    for (i = 1; i < scale + 1; i++)
                     {
-                        for (j = 0; j < scale; j++)
+                        for (j = i; j < scale + 1; j++)
                         {
-                            aMatOut[i, j] = aMatInMatrix[i,j];
+                            if (i != j)
+                            {
+                                double resist = 0;
+                                nPole = i;
+                                sPole = j;
+
+                                resist = Double.Parse(crd.CalculateResistanceDistance(scale, aArr, nPole, sPole));
+
+
+                                double now = Math.Abs(resist - pi);
+                                if (now < min)
+                                {
+                                    resistout = resist ;
+                                    
+                                        nPolego = i;
+                                    sPolego = j;
+                                    min = now;
+                                    for (i1 = 0; i1 < scale; i1++)
+                                    {
+                                        for (j1 = 0; j1 < scale; j1++)
+                                        {
+                                            aMatOut[i1, j1] = aMatInMatrix[i1, j1];
+                                        }
+                                    }
+                                }
+                            }
                         }
                     }
+                }
+                catch {
                 }
                
             }
@@ -84,11 +114,13 @@ namespace ResistAutoCalculate
                     }
                 }
             }
-            Console.Write("最小偏差"+min);
+            Console.Write("阻值"+ resistout);
 
+            Console.Write("最小偏差" + min);
+            Console.Write("进入节点" + nPolego);
+            Console.Write("跳出节点" + sPolego);
 
-
-                    Console.ReadKey();
+            Console.ReadKey();
         }
 
         private static void MatrixToArr(int scale, double[] aMat, double[,] aMatInMatrix)
